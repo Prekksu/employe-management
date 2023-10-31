9eaf9af80968e5a3209e45201358091645f71015
Novrial Sandi <122582669+Prekksu@users.noreply.github.com>
2023-10-31 15:57:09 +0700

    fix

--- projects/client/src/components/UserList.jsx
+++ projects/client/src/components/UserList.jsx
@@ -35,73 +35,75 @@ const UserList = ({ val, getUser, admin }) => {
 		}
 	}
 	return (
-		<div className="user-list-container">
-			<div className="uk-child-width-expand@s grid-container " uk-grid="true">
-				<div>
-					<img
-						className="uk-border-circle"
-						style={{ width: "50px", height: "50px" }}
-						src={`${process.env.REACT_APP_API_BASE_URL}/${val?.avatar_url}`}
-						alt="Avatar"
-					/>
-					{"  "}
-					{val?.fullname}
-				</div>
-
-				<div>{val?.email}</div>
-
-				<div>{val?.phone_number}</div>
-
-				<div>{val?.company?.company_name}</div>
-
-				<div>{val?.position?.position}</div>
-				<div>{val?.role}</div>
-				<div>
-					<a href>
-						<span uk-icon="icon: more"></span>
-					</a>
-					<div uk-dropdown="mode: click">
-						<ul class="uk-nav uk-dropdown-nav">
-							<li style={{ marginBottom: "10px" }}>
-								<div
-									style={{ cursor: "pointer" }}
-									onClick={toggleEditUserModal}
-								>
-									Edit
-								</div>
-							</li>
-
-							{admin?.role !== "S_ADMIN" ? null : (
-								<>
-									<li class="uk-nav-divider"></li>
-									<li style={{ marginBottom: "10px" }}>
-										<div
-											style={{ cursor: "pointer" }}
-											onClick={toggleDeleteUserModal}
-										>
-											Delete{" "}
-										</div>
-									</li>
-								</>
-							)}
-						</ul>
+		<>
+			<div className="user-list-container">
+				<div className="uk-child-width-expand@s grid-container " uk-grid="true">
+					<div>
+						<img
+							className="uk-border-circle"
+							style={{ width: "50px", height: "50px" }}
+							src={`${process.env.REACT_APP_API_BASE_URL}/${val?.avatar_url}`}
+							alt="Avatar"
+						/>
+						{"  "}
+						{val?.fullname}
 					</div>
-					<ModalEditUSer
-						isOpen={editUserModal}
-						toggleModal={toggleEditUserModal}
-						val={val}
-						getUser={getUser}
-						admin={admin}
-					/>
-					<ModalDeleteUser
-						isOpen={deleteUserModal}
-						toggleModal={toggleDeleteUserModal}
-						deleteUser={deleteUser}
-						val={val}
-					/>
+
+					<div>{val?.email}</div>
+
+					<div>{val?.phone_number}</div>
+
+					<div>{val?.company?.company_name}</div>
+
+					<div>{val?.position?.position}</div>
+					<div>{val?.role}</div>
+					<div>
+						<a href>
+							<span uk-icon="icon: more"></span>
+						</a>
+						<div uk-dropdown="mode: click">
+							<ul class="uk-nav uk-dropdown-nav">
+								<li style={{ marginBottom: "10px" }}>
+									<div
+										style={{ cursor: "pointer" }}
+										onClick={toggleEditUserModal}
+									>
+										Edit
+									</div>
+								</li>
+
+								{admin?.role !== "S_ADMIN" ? null : (
+									<>
+										<li class="uk-nav-divider"></li>
+										<li style={{ marginBottom: "10px" }}>
+											<div
+												style={{ cursor: "pointer" }}
+												onClick={toggleDeleteUserModal}
+											>
+												Delete{" "}
+											</div>
+										</li>
+									</>
+								)}
+							</ul>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
+			<ModalEditUSer
+				isOpen={editUserModal}
+				toggleModal={toggleEditUserModal}
+				val={val}
+				getUser={getUser}
+				admin={admin}
+			/>
+			<ModalDeleteUser
+				isOpen={deleteUserModal}
+				toggleModal={toggleDeleteUserModal}
+				deleteUser={deleteUser}
+				val={val}
+			/>
+		</>
 	);
 };
 
